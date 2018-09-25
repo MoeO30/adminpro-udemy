23c9c9c4d1f522d54ea87327c1ee665b505a42bb
Edgar Carrillo Moreno <ufosmxl@gmail.com>
2018-09-25 13:47:11 -0700

    modificaciones en rutas

--- src/app/app.router.ts
+++ src/app/app.router.ts
@@ -9,14 +9,6 @@ import { RegisterComponent } from './login/register.component';
 
 
 const appRoutes: Routes = [
-    {path: '', component: PagesComponent,
-    children: [
-        {path: 'dashboard', component: DashboardComponent},
-        {path: 'progress', component: ProgresComponent},
-        {path: 'grafica1', component: Graficas1Component},
-        {path: '', redirectTo: '/dashboard', pathMatch: 'full'},
-    ]
-},
 {path: 'login', component: LoginComponent},
 {path: 'register', component: RegisterComponent},
     {path: '**', component: NopagefoundComponent},
